e99a6ed1cb2df3feac97f6ec720bd11a16121f70
katarinaperovic <kataleja22@gmail.com>
2024-09-10 02:14:19 +0200

    fixed izgled

--- src/views/ParfemiView.vue
+++ src/views/ParfemiView.vue
@@ -3,21 +3,56 @@
       <h2>Parfemi</h2>
       <span class="nemaparfema" v-if="filteredParfemi.length === 0">Nema parfema</span>
       <div class="card-container">
-        <div v-for="parfem in filteredParfemi" :key="parfem.id" class="card">
+        <div v-for="parfem in filteredParfemi" :key="parfem.id" :class="['card', { 'card-nedostupan': !parfem.par_dostupan }]">
+
           <div class="image-container">
             <img :src="'https://localhost:44333/resources' + parfem.par_slika" class="cokolada-image" alt="Logo fabrike">
+            <h3 v-show="!parfem.par_dostupan" class="nedostupan">NEDOSTUPAN</h3>
+            <h3 v-if="cene[parfem.id] && cene[parfem.id].sc_cena && parfem.par_dostupan"  class="cenaUgao">
+           {{ cene[parfem.id].sc_cena }} rsd</h3>
+
+       <h3 class="mililitrazaUgao">{{ parfem.par_mililitraza }}ml</h3>
+
+
           </div>
           <div class="card-content">
             <strong>{{ parfem.par_naziv }}</strong>
+
             <div class="cokolada-details">
-              <p>Kategorija: {{ parfem.par_kp_id === 1 ? 'ženski' : parfem.par_kp_id === 2 ? 'muški' : parfem.par_kp_id === 3 ? 'unisex' : 'N/A' }}</p>
-              <p>Vrsta: {{ parfem.par_vp_id === 1 ? 'cvetni' : parfem.par_vp_id === 2 ? 'orijentalni' : parfem.par_vp_id === 3 ? 'citrusni' : parfem.par_vp_id === 4 ? 'drvenasti' : 'N/A' }}</p>
-              <p>Tip: {{ parfem.par_tp_id === 1 ? 'zimski' : parfem.par_tp_id === 2 ? 'prolećni' : parfem.par_tp_id === 3 ? 'letnji' : parfem.par_tp_id === 4 ? 'jesenji' : 'N/A' }}</p>
-              <p>Mililitraža: {{ parfem.par_mililitraza }}ml</p>
-              <p>Količina: {{ parfem.par_kolicina }}</p>
-              <p>Opis: {{ parfem.par_opis }}</p>
-              <p>Dostupnost: {{ parfem.par_dostupan ? 'Dostupan' : 'Nedostupan' }}</p>
-            </div>
+          <div class="category-vrsta-container">
+            <p :class="{
+              'kat-zen': parfem.par_kp_id === 1,
+              'kat-muski': parfem.par_kp_id === 2,
+              'kat-unisex': parfem.par_kp_id === 3
+            }">
+            {{ parfem.par_kp_id === 1 ? 'ženski' : parfem.par_kp_id === 2 ? 'muški' : parfem.par_kp_id === 3 ? 'unisex' : 'N/A' }}
+          </p>
+
+            <p :class="{
+              'vrsta-cvetni': parfem.par_vp_id === 1,
+              'vrsta-orijentalni': parfem.par_vp_id === 2,
+              'vrsta-citrusni': parfem.par_vp_id === 3,
+              'vrsta-drvenasti': parfem.par_vp_id === 4
+            }">
+            {{ parfem.par_vp_id === 1 ? 'cvetni' : parfem.par_vp_id === 2 ? 'orijentalni' : parfem.par_vp_id === 3 ? 'citrusni' : parfem.par_vp_id === 4 ? 'drvenasti' : 'N/A' }}
+          </p>
+
+          <p :class="{
+              'tip-zimski': parfem.par_tp_id === 1,
+              'tip-prolecni': parfem.par_tp_id === 2,
+              'tip-letnji': parfem.par_tp_id === 3,
+              'tip-jesenji': parfem.par_tp_id === 4
+            }">
+            </p>
+
+          </div>
+
+          <p>Količina: {{ parfem.par_kolicina }}</p>
+          <p class="opis"> {{ parfem.par_opis }}</p>
+          
+        </div>
+
+           
             <div class="button-container">
               <button @click="editParfem(parfem)" v-if="korisnikUloga === 'menadzer' && String(korisnikFabrikaId) === String(fabrikaId)">Izmeni</button>
               <button @click="editKolicina(parfem)" v-if="korisnikUloga === 'radnik' && String(korisnikFabrikaId) === String(fabrikaId)">Izmeni količinu</button>
@@ -35,13 +70,17 @@
           <form @submit.prevent="saveParfem" class="formm">
             <label>Ime:</label>
             <input v-model="editedParfem.par_naziv" required>
-            <span v-if="errors.cena" class="errors">{{ errors.cena }}</span>
             <label>Mililitraža:</label>
             <input v-model.number="editedParfem.par_mililitraza" required @input="validateMililitraza" title="Molimo unesite broj veći od 0">
             <span v-if="errors.mililitraza" class="errors">{{ errors.mililitraza }}</span>
             <label>Količina:</label>
             <input v-model.number="editedParfem.par_kolicina" required @input="validateKolicina" title="Molimo unesite broj >=0">
             <span v-if="errors.kolicina" class="errors">{{ errors.kolicina }}</span>
+             <!-- Novo polje za unos cene -->
+      <label>Cena:</label>
+      <input v-model.number="editedCena.sc_cena" required @input="validateCena" title="Molimo unesite broj >=0" />
+
+      <span v-if="errors.cena" class="errors">{{ errors.cena }}</span>
             <label>Opis:</label>
             <input v-model="editedParfem.par_opis" required>
             <label>Slika:</label>
@@ -96,8 +135,10 @@ const props = defineProps({
 });
 
 const parfemi = ref([]);
+const cene = ref([]);
 const editingParfem = ref(null);
 const editedParfem = ref({});
+const editedCena = ref({});
 
 const deletingParfem = ref(null);
 const deletedParfem = ref({});
@@ -120,7 +161,7 @@ const errors = ref({
   editingKolicina.value = null;
 }
     const validateCena = () => {
-  if (editedCokolada.value.cena <= 0) {
+  if (editedCena.value.sc_cena <= 0) {
     errors.value.cena = 'Cena mora biti veća od 0';
     return false; 
   } else {
@@ -178,12 +219,26 @@ async function loadParfemi() {
   try {
     const response = await axios.get('https://localhost:44333/api/parfem');
     parfemi.value = response.data;
+ 
+    await Promise.all(
+      parfemi.value.map(async (parfem) => {
+        if (!cene.value[parfem.id]) {
+          const ceneResponse = await axios.get(`https://localhost:44333/api/stavka-cenovnika/parfemId/${parfem.id}`);
+          if (ceneResponse.data) {
+            cene.value[parfem.id] = ceneResponse.data; // Obezbedi da podatak postoji
+          }
+        }
+      })
+    );
+
+    console.log(cene.value); 
+
   } catch (error) {
     console.error('Greška prilikom učitavanja parfema:', error);
   }
 }
 
-function editParfem(parfem) {
+async function editParfem(parfem) {
   editingParfem.value = parfem;
   editedParfem.value = {
       par_naziv: parfem.par_naziv,
@@ -198,6 +253,11 @@ function editParfem(parfem) {
       par_tp_id: parfem.par_tp_id,
       par_kp_id: parfem.par_kp_id
   };
+
+  const ceneResponse = await axios.get(`https://localhost:44333/api/stavka-cenovnika/parfemId/${parfem.id}`);
+  if (ceneResponse.data) {
+    editedCena.value = ceneResponse.data;  // Povlačenje trenutne cene za uređivanje
+  }
 }
 function editKolicina(parfem) {
   editingKolicina.value = parfem;
@@ -222,31 +282,40 @@ function pripremaDeleteParfem(parfem) {
   };
 }
 
-function saveParfem() {
-  //const cenaValid = validateCena();
+async function saveParfem() {
+  const cenaValid = validateCena();
   const mililitrazaValid = validateMililitraza();
   const kolicinaValid = validateKolicina();
  
-  /*if (!cenaValid || !gramazaValid || !kolicinaValid) {
-    return;
-  }*/
-  if (!mililitrazaValid || !kolicinaValid) {
+  if (!cenaValid || !mililitrazaValid || !kolicinaValid) {
     return;
   }
+  
 
   if (editedParfem.value.kolicina > 0) {
     editedParfem.value.par_dostupan = true;
   }
-  axios.put(`https://localhost:44333/api/parfem/${editingParfem.value.id}`, editedParfem.value)
-    .then(response => {
-      loadParfemi();
-      editingParfem.value = null;
-      editedParfem.value = {};
-      errors.value.cena = null;
-      errors.value.mililitraza = null;
-      errors.value.kolicina = null;
-    })
-    .catch(error => console.error(error));
+  console.log(editedCena.value);
+  try {
+    // Ažuriranje podataka parfema
+    await axios.put(`https://localhost:44333/api/parfem/${editingParfem.value.id}`, editedParfem.value);
+
+    
+
+    // Ažuriranje cene u stavci cenovnika
+    await axios.put(`https://localhost:44333/api/stavka-cenovnika/${editedCena.value.id}`, editedCena.value);
+
+    cene.value[editingParfem.value.id].sc_cena = editedCena.value.sc_cena;
+
+    loadParfemi();
+    editingParfem.value = null;
+    editedParfem.value = {};
+    errors.value.cena = null;
+    errors.value.mililitraza = null;
+    errors.value.kolicina = null;
+  }catch (error) {
+    console.error('Greška prilikom čuvanja:', error);
+  }
 }
 
 function saveKolicina() {
@@ -348,7 +417,7 @@ h2 {
 
 .card {
   width: 320px; 
-  background-color: #fefefe;
+  background-color: #ddf9f6;
   border-radius: 15px;
   overflow: hidden;
   box-shadow: 0 8px 16px rgba(0, 0, 0, 0.1);
@@ -363,26 +432,72 @@ h2 {
 }
 
 .image-container {
-  background-color: #f9f9f9;
+  position: relative; 
+  background-color: #f8efef;
   padding: 15px;
-  border-bottom: 1px solid #ddd;
+  
+}
+
+.nedostupan {
+  position: absolute;
+  top: 10px; /* Udaljenost od vrha */
+  left: 10px; /* Udaljenost od leva */
+  background-color: rgba(255, 0, 0, 0.7); /* Poluproziran crveni pozadinski boja */
+  color: white; /* Boja teksta */
+  padding: 5px 10px;
+  border-radius: 5px;
+  font-size: 1.2rem;
+  font-weight: bold;
+}
+
+
+.cenaUgao {
+  position: absolute;
+  top: 10px; /* Udaljenost od vrha */
+  left: 10px; /* Udaljenost od leva */
+  background-color: rgba(255, 174, 0, 0.834); /* Poluproziran crveni pozadinski boja */
+  color: white; /* Boja teksta */
+  padding: 5px 10px;
+  border-radius: 5px;
+  font-size: 1.2rem;
+  font-weight: bold;
+}
+
+.mililitrazaUgao {
+  position: absolute;
+  bottom: 10px; /* Udaljenost od dna*/
+  right: 10px; /* Udaljenost od leva */
+  background-color: rgba(82, 8, 39, 0.834); /* Poluproziran crveni pozadinski boja */
+  color: white; /* Boja teksta */
+  padding: 5px 10px;
+  border-radius: 5px;
+  font-size: 1.2rem;
+  font-weight: bold;
 }
 
 .cokolada-image {
   width: 100%;
-  height: 220px; /* Adjusted for better visuals */
+  height: 150px; /* Adjusted for better visuals */
   object-fit: cover;
   border-radius: 10px;
 }
 
 .card-content {
+ 
   padding: 20px;
   flex-grow: 1; /* Make the content fill available space */
-  color: #333;
+  color: #fffdfd;
   display: flex;
   flex-direction: column;
   justify-content: space-between;
 }
+
+.card-nedostupan {
+  background-color: #d3d3d3; /* Svetlo siva boja */
+  color: #7f7f7f; /* Tamnija boja za tekst */
+  
+}
+
 
 .card-content strong {
   font-size: 1.4rem;
@@ -390,18 +505,24 @@ h2 {
   margin-bottom: 15px;
 }
 
+
+
 .cokolada-details p {
   font-size: 1rem;
-  color: #555;
-  margin: 8px 0;
+  color: #060606;
+  font-style: oblique;
+  margin: 1px 0;
+  font-weight: bold;
   display: flex;
   align-items: center;
+ 
 }
 
 .cokolada-details p::before {
-  content: '•'; /* Use a dot for better separation */
+ 
   color: #4f2a06;
-  margin-right: 8px;
+ 
+ 
 }
 .nemaparfema{
     color: #ddd;
@@ -443,11 +564,13 @@ h2 {
 }
 
 
+
+
 .errors{
   color: red;
   grid-column: span 2;
   margin-left: 215px;
-  
+  margin-top: -30px !important;
 } 
 
 /* For mobile responsiveness */
@@ -466,7 +589,7 @@ h2 {
   left: 0;
   top: 0;
   width: 100%;
-  height: 100%;
+ height: 100%;
   
   background-color: rgba(142, 122, 126, 0.534); 
 
@@ -507,6 +630,82 @@ h2 {
   color: #000;
 }
 
+.kat-zen {
+  background-color: pink;
+ 
+ 
+}
+
+.kat-muski {
+  background-color: lightblue;
+ 
+}
+
+.kat-unisex {
+  background: linear-gradient(to right, pink, lightblue);
+}
+
+.vrsta-cvetni {
+  background: linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url('@/assets/cvetni.jpg');
+  background-size: cover;
+  background-position: center;
+}
+
+
+.vrsta-orijentalni {
+  background: linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url('@/assets/orijentalni.jpg');
+  background-size: cover;
+  background-position: center;
+}
+
+.vrsta-citrusni{
+  background: linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url('@/assets/citrusni.jpg');
+  background-size: cover;
+  background-position: center;
+}
+
+.vrsta-drvenasti{
+  background: linear-gradient(rgba(255, 255, 255, 0.5), rgba(255, 255, 255, 0.5)), url('@/assets/drvenasti.jpg');
+  background-size: cover;
+  background-position: center;
+}
+
+
+.tip-zimski {
+  background-image: url('@/assets/zima.png');
+  background-size: cover;
+  background-position: center;
+}
+
+.tip-prolecni{
+  background-image: url('@/assets/prolece.png');
+  background-size: cover;
+  background-position: center;
+}
+
+.tip-letnji{
+  background-image: url('@/assets/leto.png');
+  background-size: cover;
+  background-position: center;
+}
+
+.tip-jesenji{
+  background-image: url('@/assets/jesen.png');
+  background-size: cover;
+  background-position: center;
+}
+.opis {
+  font-size: 18px !important;  
+  color: #a50404 !important;   
+  font-style: normal !important; 
+  margin: 10px 0 !important;  
+  font-weight: normal !important;
+  display: block !important;   
+  align-items: normal !important; 
+}
+
+
+
 .save-button {
   margin-top: 10px;
   padding: 10px;
@@ -532,19 +731,56 @@ h2 {
   font-size: 20px;
   
 }
-.fo{
-  display: grid;
-  grid-template-columns: 1fr 1fr;
-  gap: 8px;
-  row-gap: 30px;
-  width: 70%;
-  color: #f3deee;
-  font-size: 20px;
-  
-}
+
 .formm input:disabled {
   background-color: #4f4f4f;
   color: #edeaea;
 }
+
+.cokolada-details {
+  display: flex;
+  flex-direction: column;
+  margin-bottom: 15px;
+}
+
+.category-vrsta-container {
+  display: flex;
+  justify-content:center; /* Razmak između kategorije i vrste */
+  align-items: center; /* Poravnanje u istoj liniji */
+  
+  padding: 8px;
+
+  border-radius: 10px; /* Zaobljene ivice */
+  font-size: 1.2rem; /* Povećanje veličine fonta */
+}
+
+.category-vrsta-container p {
+  flex: 1;
+  min-width: 90px; /* Primer širine, prilagodite po potrebi */
+  height: 50px; /* Primer visine, prilagodite po potrebi */
+  background-size: cover;
+  background-position: center;
+  text-align: center;
+  border-radius: 10px;
+  margin: 0 4px;
+  margin-top: -10px;
+ 
+}
+
+
+.kat-zen, .kat-muski, .kat-unisex {
+  font-size: 1.2rem; /* Povećanje veličine fonta */
+}
+
+.vrsta-cvetni, .vrsta-orijentalni, .vrsta-citrusni, .vrsta-drvenasti {
+  font-size: 1.2rem; /* Povećanje veličine fonta */
+}
+
+.tip-zimski, .tip-jesenji, .tip-prolecni, .tip-letnji{
+  font-size: 1.2rem; /* Povećanje veličine fonta */
+}
+
+/* Dodaj granicu oko celog elementa */
+
 
 </style>
